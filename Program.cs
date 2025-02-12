665ae91aceb866a996c0e70dcdfc192262748d31
wsuduce <wsuduce@gmail.com>
2025-02-11 16:13:33 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Serve static files from wwwroot
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
